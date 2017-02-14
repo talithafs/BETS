ed184d3d6ae158701dc443d3ee869d70dfbb6073
jonatha azevedo <johnmasterchip@gmail.com>
2017-02-14 17:34:42 -0200

    Formato da data!

--- R/BETS.get.R
+++ R/BETS.get.R
@@ -37,6 +37,12 @@
 
 BETS.get = function(code, from = "", to = "", data.frame = FALSE, frequency = NULL){
   
+     date_to   = strsplit(to,split="-")
+     to = paste0(date_to[[1]][2],"/",date_to[[1]][3],"/",date_to[[1]][1]) 
+  
+     date_from = strsplit(from,split="-")
+     from = paste0(date_from[[1]][2],"/",date_from[[1]][3],"/",date_from[[1]][1])
+  
     if(!grepl("ST_",code)){
       
       code = as.numeric(code)
